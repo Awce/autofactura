f6c131d67407df246f5bb5a85237cf098baef46a
Andres Amaya Diaz <andres.amaya.diaz@gmail.com>
2014-01-15 20:18:15 -0600

    Beta 1 Funcion Emitir por Terminarse

--- lib/autofactura.rb
+++ lib/autofactura.rb
@@ -1,5 +1,6 @@
 require "uri"
 require "net/http"
+require "date"
 
 module Autofactura
   
@@ -17,6 +18,26 @@ module Autofactura
       
     end
     
+    # Emite un Comprobatnte CFDi de AutoFactura.com
+    # --------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------
+    def emitir(comprobante)
+      
+      return if invalid?
+      
+      # TODO
+      
+      params = {
+        'data[metodo]' => 'emitir',
+        'data[userg]' => self.user,
+        'data[sucursalg]' => self.sucursal,
+        'data[Comprobante]' => '',
+      }
+      #request = Net::HTTP.post_form(URI.parse(self.url), params)
+      
+    end
+    # Termina Metodo Ticket
+    
     # Emite un ticket de Autofacturacion
     # --------------------------------------------------------------------------------
     # exito = 1 = Éxito, 0 = Fracaso
@@ -28,7 +49,7 @@ module Autofactura
       
       return if invalid?
       
-      
+      # TODO
       
       params = {
         'data[metodo]' => 'ticket',
@@ -36,7 +57,7 @@ module Autofactura
         'data[sucursalg]' => self.sucursal,
         'data[nota]' => '',
       }
-      request = Net::HTTP.post_form(URI.parse(self.url), params)
+      #request = Net::HTTP.post_form(URI.parse(self.url), params)
       
     end
     # Termina Metodo Ticket
@@ -80,7 +101,7 @@ module Autofactura
       params = {
         'data[metodo]' => 'cancelar',
         'data[userg]' => self.user,
-        'data[sucursalg]' => self.sucursal
+        'data[sucursalg]' => self.sucursal,
         'data[id]' => comprobante_uuid
       }
       request = Net::HTTP.post_form(URI.parse(self.url), params)
@@ -115,6 +136,219 @@ module Autofactura
     
   end
   # Termina Clase Serie
+  
+  # Clase Comprobante
+  class Comprobante
+    
+    attr_accessor :fecha, :serie, :tipoDeComprobante, :condicionesDePago, :formaDePago, :metodoDePago, :numerocta, :version, :tipoCambio, :moneda, :decimales, :Receptor, :Conceptos, :Impuestos, :ret_iva_cant, :tras_ieps_cant, :tras_iva_cant, :ret_isr_cant, :Addenda, :subTotal, :total, :descuento, :descuento_porcentual
+    
+    def initialize(params)
+      
+      # Generales
+      self.fecha = DateTime.parse(Time.now.to_s).strftime("%Y-%m-%dT%H:%M:%S").to_s
+      self.serie = params[:serie]
+      self.tipoDeComprobante = params[:tipoDeComprobante]
+      self.condicionesDePago = params[:condicionesDePago].blank? ? "CONTADO" : params[:condicionesDePago]
+      self.formaDePago = params[:formaDePago].blank? ? "PAGO EN UNA SOLA EXHIBICION" : params[:formaDePago]
+      self.metodoDePago = params[:metodoDePago].blank? ? "NO IDENTIFICADO" : params[:metodoDePago]
+      self.numerocta = params[:numerocta].blank? ? "NO IDENTIFICADO" : params[:numerocta]
+      self.version = params[:version].blank? ? "3.2" : params[:version]
+      self.tipoCambio = params[:tipoCambio].blank? ? 1.00 : params[:tipoCambio]
+      self.moneda = params[:moneda].blank? ? "MXN" : params[:moneda]
+      self.decimales = params[:decimales].blank? ? 2 : params[:decimales]
+      
+      # Receptor
+      self.Receptor = Receptor.new(params[:Receptor])
+      
+      # Conceptos e Impuestos
+      # Impuestos por Concepto
+      self.subTotal = 0.00 # subTotal
+      self.Impuestos = Array.new
+      self.Conceptos = Array.new
+      #puts "------- CONCEPTOS -------"
+      params[:Conceptos].each do |conc|
+        imp = {}
+        concepto = Concepto.new(conc)
+        
+        unless concepto.tras_iva.blank?
+          imp[:tras_iva] = concepto.tras_iva
+          imp[:tras_iva_cant] = ( ( concepto.importe - concepto.descuento ) * concepto.tras_iva / 100 )
+        end
+        
+        unless concepto.tras_ieps.blank?
+          imp[:tras_ieps] = concepto.tras_ieps
+          imp[:tras_ieps_cant] = ( ( concepto.importe - concepto.descuento ) * concepto.tras_ieps / 100 )
+        end
+        
+        unless concepto.ret_iva.blank?
+          imp[:ret_iva] = concepto.ret_iva
+          imp[:ret_iva_cant] = ( ( concepto.importe - concepto.descuento ) * concepto.ret_iva / 100 )
+        end
+        
+        unless concepto.ret_isr.blank?
+          imp[:ret_isr] = concepto.ret_isr
+          imp[:ret_isr_cant] = ( ( concepto.importe - concepto.descuento ) * concepto.ret_isr / 100 )
+        end
+        
+        self.Conceptos.push(concepto)
+        self.Impuestos.push(Impuesto.new(imp))
+        
+        self.subTotal += ( concepto.importe )
+        
+      end
+      #puts "------- FIN CONCEPTOS -------"
+      
+      # Impuestos Totalizados
+      self.tras_iva_cant = 0.00
+      self.tras_ieps_cant = 0.00
+      self.ret_iva_cant = 0.00
+      self.ret_isr_cant = 0.00
+      #puts "------- IMPUESTOS -------"
+      self.Impuestos.each do |impuesto|
+        
+        unless impuesto.tras_iva_cant.blank?
+          self.tras_iva_cant += impuesto.tras_iva_cant
+        end
+        
+        unless impuesto.tras_ieps_cant.blank?
+          self.tras_ieps_cant += impuesto.tras_ieps_cant
+        end
+        
+        unless impuesto.ret_iva_cant.blank?
+          self.ret_iva_cant += impuesto.ret_iva_cant
+        end
+        
+        unless impuesto.ret_isr_cant.blank?
+          self.ret_isr_cant += impuesto.ret_isr_cant
+        end
+        
+      end
+      #puts "------- FIN IMPUESTOS -------"
+      
+      # Totales
+      self.descuento_porcentual = params[:descuento_porcentual].blank? ? 0.00 : params[:descuento_porcentual]
+      self.descuento = ( (self.subTotal * self.descuento_porcentual) / 100 )
+      self.total = self.subTotal - self.descuento + self.tras_iva_cant + self.tras_ieps_cant - self.ret_iva_cant - self.ret_isr_cant
+      
+    end
+    
+  end
+  # Termina Clase Comprobante
+  
+  # Clase Receptor
+  class Receptor
+    
+    attr_accessor :rfc, :nombre, :email, :Domicilio
+    
+    def initialize(params)
+      
+      self.rfc = params[:rfc]
+      self.nombre = params[:nombre]
+      self.email = params[:email]
+      self.Domicilio = Domicilio.new(params[:Domicilio]) # Clase Domicilio
+      
+    end
+    
+  end
+  # Termina Clase Receptor
+  
+  # Clase Domicilio
+  class Domicilio
+    
+    attr_accessor :noExterior, :noInterior, :calle, :colonia, :municipio, :estado, :pais, :codigoPostal
+    
+    def initialize(params)
+      
+      self.noExterior = params[:noExterior]
+      self.noInterior = params[:noInterior].blank? ? nil : params[:noInterior]
+      self.calle = params[:calle]
+      self.colonia = params[:colonia]
+      self.municipio = params[:municipio]
+      self.estado = params[:estado]
+      self.pais = params[:pais]
+      self.codigoPostal = params[:codigoPostal]
+      
+    end
+    
+  end
+  # Termina Clase Domicilio
+  
+  # Clase Concepto
+  class Concepto
+    
+    attr_accessor :cantidad, :unidad, :descripcion, :valorUnitario, :importe, :tras_iva, :ret_iva, :ret_isr, :tras_ieps, :tras_ieps_cant, :tras_iva_cant, :ret_iva_cant, :ret_isr_cant, :descuento, :descuento_porcentual
+    
+    def initialize(params)
+      
+      # Generales
+      self.cantidad = params[:cantidad]
+      self.unidad = params[:unidad].blank? ? "NO APLICA" : params[:unidad]
+      self.descripcion = params[:descripcion]
+      self.valorUnitario = params[:valorUnitario]
+      
+      # Importe
+      self.importe = (self.cantidad * self.valorUnitario)
+      
+      # Descuento
+      self.descuento_porcentual = params[:descuento_porcentual].blank? ? nil : params[:descuento_porcentual]
+      if params[:descuento_porcentual].blank?
+        self.descuento = 0.00
+      else
+        self.descuento = ( self.importe * self.descuento_porcentual / 100 )
+      end
+      
+      # Impuestos
+      # Impuestos Porcentaje
+      self.tras_iva = params[:tras_iva].blank? ? 0 : params[:tras_iva].to_f
+      self.ret_iva = params[:ret_iva].blank? ? 0 : params[:ret_iva].to_f
+      self.ret_isr = params[:ret_isr].blank? ? 0 : params[:ret_isr].to_f
+      self.tras_ieps = params[:tras_ieps].blank? ? 0 : params[:tras_ieps].to_f
+      # Impuestos Cantidad
+      self.tras_ieps_cant = ( ( self.importe - self.descuento ) * params[:tras_ieps] / 100 )
+      self.ret_isr_cant = ( ( self.importe - self.descuento ) * params[:ret_isr] / 100 )
+      self.ret_iva_cant = ( ( self.importe - self.descuento ) * params[:ret_iva] / 100 )
+      self.tras_iva_cant = ( ( self.importe - self.descuento ) * params[:tras_iva] / 100 )
+      
+    end
+    
+    def invalid
+      
+      if self.unidad.blank?
+        raise 'Debe especificar la unidad del concepto.'
+      end
+      
+      if self.cantidad <= 0
+        raise 'La cantidad del concepto no puede ser 0 o menor.'
+      end
+      
+      return false
+      
+    end
+    
+  end
+  # Termina Clase Concepto
+  
+  # Clase Impuesto
+  class Impuesto
+    
+    attr_accessor :tras_iva, :ret_iva, :ret_isr, :tras_ieps, :tras_ieps_cant, :ret_isr_cant, :ret_iva_cant, :tras_iva_cant
+    
+    def initialize(params)
+      
+      self.tras_iva = params[:tras_iva].blank? ? 0 : params[:tras_iva].to_f
+      self.ret_iva = params[:ret_iva].blank? ? 0 : params[:ret_iva].to_f
+      self.ret_isr = params[:ret_isr].blank? ? 0 : params[:ret_isr].to_f
+      self.tras_ieps = params[:tras_ieps].blank? ? 0 : params[:tras_ieps].to_f
+      
+      self.tras_ieps_cant = params[:tras_ieps_cant].blank? ? 0 : params[:tras_ieps_cant].to_f
+      self.ret_isr_cant = params[:ret_isr_cant].blank? ? 0 : params[:ret_isr_cant].to_f
+      self.ret_iva_cant = params[:ret_iva_cant].blank? ? 0 : params[:ret_iva_cant].to_f
+      self.tras_iva_cant = params[:tras_iva_cant].blank? ? 0 : params[:tras_iva_cant].to_f
+      
+    end
+    
+  end
+  # Termina Clase Impuesto
   
   # Clase Remision
   class Remision
--- test/autofactura_test.rb
+++ test/autofactura_test.rb
@@ -2,6 +2,65 @@ require 'test_helper'
 
 class AutofacturaTest < ActiveSupport::TestCase
   test "truth" do
+  
     assert_kind_of Module, Autofactura
   end
+  
+  test "comprobante" do
+    
+    comp = { 
+      :serie => "SERIE",
+      :tipoDeComprobante => "ingreso",
+      :condicionesDePago => "CONTADO2",
+      :formaDePago => "PAGO EN UNA SOLA EXHIBICION2",
+      :metodoDePago => "NO IDENTIFICADO2",
+      :numerocta => "NO IDENTIFICADO2",
+      :version => "3.2",
+      :tipoCambio => 1,
+      :moneda => "MXN",
+      :decimales => 2,
+      :Receptor => {
+        :rfc => "RFC",
+        :nombre => "RAZON SOCIAL RECEPTOR",
+        :email => "andres.amaya.diaz@gmail.com",
+        :Domicilio => {
+          :noExterior => "103",
+          :calle => "San Juan",
+          :colonia => "Colonia",
+          :municipio => "Monterrey",
+          :estado => "Nuevo Leon",
+          :pais => "Mexico",
+          :codigoPostal => "66250"
+        } # Fin Domicilio
+      }, # Fin Receptor
+      :Conceptos => [{
+          :cantidad => 1,
+          :unidad => "NO APLICA2",
+          :descripcion => "Primer Concepto",
+          :valorUnitario => 100,
+          :descuento_porcentual => 0,
+          :ret_iva => 0,
+          :tras_ieps => 0,
+          :tras_iva => 16,
+          :ret_isr => 0
+        },{
+          :cantidad => 1,
+          :unidad => "NO APLICA2",
+          :descripcion => "Segundo Concepto",
+          :valorUnitario => 1000,
+          :descuento_porcentual => 0,
+          :ret_iva => 0,
+          :tras_ieps => 0,
+          :tras_iva => 16,
+          :ret_isr => 0
+        }],
+        :Addenda => ""
+     }
+     
+     comprobante = Autofactura::Comprobante.new( comp )
+     puts "COMPROBANTE: " + comprobante.to_json
+     assert_not_nil(comprobante, "El Comprobante no se ha creado.")
+     
+  end
+  
 end
